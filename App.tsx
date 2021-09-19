2cdc62bb44b5d106774b996e6cb9fe49520c8324
dovigod <inaki96@naver.com>
2021-09-19 15:01:33 +0900

    load font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, NativeModule } from 'react-native';
+import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import { Asset } from 'expo-asset';
+import { Ionicons } from '@expo/vector-icons';
 
-const cacheImages = (images: Array<string>) =>
+const cacheImages = (images: Array<string> | any) =>
 	images.map((image: string) => {
 		if (typeof image === 'string') {
 			return Image.prefetch(image);
@@ -13,14 +15,20 @@ const cacheImages = (images: Array<string>) =>
 		}
 	});
 
+const cacheFonts = (fonts: any) => fonts.map((font: any) => (font) => Font.loadAsync(font));
+
 export default function App() {
 	const [isReady, setIsReady] = useState(false);
-	const loadAssets = async () => {
+	const loadAssets = (): any => {
 		const images = cacheImages([
 			'https://images.unsplash.com/photo-1615387116460-b5ef38c83fbc?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60',
 			require('./assets/splash.png')
 		]);
+
+		const fonts = cacheFonts([Ionicons.font]);
 		console.log(images);
+		//Because the Promise.all will wait for it :)
+		return Promise.all([...images, ...fonts]);
 	};
 	const onFinish = () => {
 		setIsReady(true);
@@ -31,3 +39,12 @@ export default function App() {
 		<AppLoading startAsync={loadAssets} onFinish={onFinish} onError={(e) => console.log(e)} />
 	);
 }
+
+/*
+Image.prefetch() => 이미지 미리 로딩
+asset <- asset sys에 접근 가능하게
+Apploading => preloading
+prop{
+  startAsync =? promise return func
+}
+*/
