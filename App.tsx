ac6bb141f9db198b40d198a4216e94c7338c20a2
dovigod <inaki96@naver.com>
2021-09-19 14:47:19 +0900

    preloading

--- App.tsx
+++ App.tsx
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { Asset } from 'expo-asset';
+
+const cacheImages = (images: Array<string>) =>
+	images.map((image: string) => {
+		if (typeof image === 'string') {
+			return Image.prefetch(image);
+		} else {
+			return Asset.fromModule(image).downloadAsync();
+		}
+	});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [isReady, setIsReady] = useState(false);
+	const loadAssets = async () => {
+		const images = cacheImages([
+			'https://images.unsplash.com/photo-1615387116460-b5ef38c83fbc?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60',
+			require('./assets/splash.png')
+		]);
+		console.log(images);
+	};
+	const onFinish = () => {
+		setIsReady(true);
+	};
+	return isReady ? (
+		<Text>ready</Text>
+	) : (
+		<AppLoading startAsync={loadAssets} onFinish={onFinish} onError={(e) => console.log(e)} />
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
